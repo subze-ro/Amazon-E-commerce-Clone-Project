ff3f3fcd3f5a2e2a324c4a907c8d6a6bfb8630e8
Alex <alexnjoroge102@gmail.com>
2025-02-07 10:54:35 +0300

    Learn promises and convert code to promises

--- data/cart.js
+++ data/cart.js
@@ -78,3 +78,18 @@ export function addToCart (productId) {
   saveToStorage();
 
 }
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+
+    console.log(xhr.response)
+       
+    fun(); 
+    
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send(); //this is asychronous and will not wait for a response
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,13 +3,55 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
 
-loadProducts(() => {
+Promise.all([
+  new Promise((resolve) => {  
+    loadProducts(() => {    
+      resolve('value1');
+    });  
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {});  
+    resolve();
+  }) 
+
+]).then((values) => {
+  console.log(values)
   renderOrderSummary();
   renderPaymentSummary();
 });
 
 
+/*
+// an example of using Promises which help us write asychronous code better and avoid nesting issues
+new Promise((resolve) => {  
+  loadProducts(() => {    
+    resolve('value1');
+  });
+
+}).then((value) => {
+
+  console.log(value )
+  return new Promise((resolve) => {
+    loadCart(() => {});  
+    resolve();
+  });  
+
+}).then(() => {
+  renderOrderSummary();
+  renderPaymentSummary(); 
+});
+
+//an example of using callbacks which create a lot of nesting: code inside code inside code
+
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary(); 
+  });  
+});
 
 
+*/
